a092de439ec781e9e66cf70ef6957f59f6a9af7f
Anna Ruiz <anna.ruiz@free.fr>
2023-09-19 16:51:18 +0200

    difference of squares

---
+++ TP-01-Echauffement/01-differenceOfSquares.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <math.h>
+
+uint16_t squareOfSum(uint16_t n){
+    uint16_t sum = 0;
+    for (int i=1; i<=n; i++){
+        sum += i;
+    }
+    return pow(sum,2);
+}
+
+uint16_t sumOfSquares(uint16_t n){
+    uint16_t sum = 0;
+    for (int i=1; i<=n; i++){
+        sum += pow(i,2);
+    }
+    return sum;
+}
+
+uint16_t difference(uint16_t n){
+    return (squareOfSum(n) - sumOfSquares(n));
+}
+
+int main (void){
+    uint16_t number= 10;
+    printf("Difference between squareOfSum of first %"PRIu16, number);
+    printf(" natural numbers and its sumOfSquares : %"PRIu16"\n", difference(number));
+}
