31841da1f1f80e9b8d893fdb7040e81a9158eda3
Anna Ruiz <anna.ruiz@free.fr>
2023-09-19 15:20:36 +0200

    factorielle qui fonctionne jusqu'à 15

--- TP-01-Echauffement/01-factorielle.c
+++ TP-01-Echauffement/01-factorielle.c
@@ -4,12 +4,12 @@
 #include <stdbool.h> //bool
 #include <stdint.h> //uint8_t, uint64_t,...
 
-typedef short TypeEntier;
+typedef unsigned long long TypeEntier;
 TypeEntier factorielle(TypeEntier);
 
 TypeEntier factorielle(TypeEntier n){
     TypeEntier facto = 1;
-    for (int i=1; i<n ; i++){
+    for (int i=1; i<=n ; i++){
         facto*=i;
     }
     return facto;
@@ -17,8 +17,14 @@ TypeEntier factorielle(TypeEntier n){
 
 
 int main (void){
-    TypeEntier nombre = 6;
-
-    printf("Le factoriel de %u est : %u \n", nombre, factorielle(nombre));
-
+    TypeEntier nombre = 15;
+    for (int i=1; i <= nombre; i++){
+        printf("Le factoriel de %u est : %llu \n",i, factorielle(i));
+    }
 }
+
+/*
+On remarque qu'avec le type "short" on n'obtient pas les bons résultats pour factorielle à partir de 8.
+Cela est du à un problème de mémoire ? La mémoire allouée à ce type est trop petite.
+Avec le type "unsigned long long" on obtient le bon résultat
+ */
