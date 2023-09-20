068206d1c461ae7817eb938577bd2cd3a26521f7
Anna Ruiz <anna.ruiz@free.fr>
2023-09-20 10:53:04 +0200

    exercice matrices

--- TP-02-pointeurs/02-matrices.c
+++ TP-02-pointeurs/02-matrices.c
@@ -5,7 +5,7 @@
 
 #define SIZE 5
 
-void matrix_mult(int64_t matR[SIZE][SIZE], int64_t mat1[][SIZE],int64_t mat2[][SIZE]){
+void matrix_mult(int64_t matR[][SIZE], int64_t mat1[][SIZE],int64_t mat2[][SIZE]){
     for (int i=0; i<SIZE; i++){
         for(int j=0; j<SIZE;j++){
             int64_t somme = 0;
@@ -27,9 +27,12 @@ void matrix_print (int64_t matR[SIZE][SIZE]){
 }
 
 int main(void){
+    // en C on ne peut omettre qu'une seule dimension. 
+    //On n'aurait pas pu faire l'inverse. 
+    //Il faut mettre la dimension la plus interne
     int64_t matrice1[][SIZE]={{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5}};
     int64_t matrice2[][SIZE]={{6,7,8,9,10},{6,7,8,9,10},{6,7,8,9,10},{6,7,8,9,10},{6,7,8,9,10}};
-    int64_t matriceResultat [SIZE][SIZE];
+    int64_t matriceResultat [SIZE][SIZE]; // c'est un tableau donc c'est l'adresse de la première case
 
     matrix_mult(matriceResultat,matrice1,matrice2);
     matrix_print(matriceResultat);
@@ -38,7 +41,5 @@ int main(void){
 }
 
 /*
-sans pointeur, on ne peut pas réutiliser matrix_mult. 
-Une fois la fonction terminée l'espace mémoire va se supprimer. 
-On n'a pas modifié matriceResultat mais sa copie.
+pb : taille ? 
 */
