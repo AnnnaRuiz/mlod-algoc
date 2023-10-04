c4de80b4e61a786f90baa29ac723e4f5e32e55d2
Anna Ruiz <anna.ruiz@free.fr>
2023-10-04 10:28:49 +0200

    V1 terminée

--- TP-04-liste-chainee/V1/linkedListOfString.c
+++ TP-04-liste-chainee/V1/linkedListOfString.c
@@ -7,9 +7,7 @@ void afficheElement(Element e){
     printf("%s ",(char*)e);
 }
 
-void detruireElement(Element e){
-	e = NULL;
-}
+void detruireElement(Element e){}
 
 bool equalsElement(Element e1, Element e2){
 	return strcmp((char*)e1,e2)==0;
